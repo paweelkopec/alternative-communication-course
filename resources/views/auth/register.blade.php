f477191f8549fb85bab6fc01678db8223a7db239
pawel <pawel@pawel-Vostro-3700>
2016-04-12 22:54:59 +0200

    Poprawiono rejestracje.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">Rejestracja</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
+                            <label class="col-md-4 control-label">Adres Email</label>
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}" required="">
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -23,13 +20,10 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
+                            <label class="col-md-4 control-label">Hasło</label>
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
+                                <input type="password" class="form-control" name="password" required="">
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -39,11 +33,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Confirm Password</label>
-
+                            <label class="col-md-4 control-label">Powtórz Hasło</label>
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
-
+                                <input type="password" class="form-control" name="password_confirmation" required="">
                                 @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
@@ -55,7 +47,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i>Register
+                                    <i class="fa fa-btn fa-user"></i>Rejestruj
                                 </button>
                             </div>
                         </div>
